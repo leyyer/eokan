6a81c8201193be285783a63281596f3f09508397
leyyer <surenyi@gmail.com>
2013-04-02 09:33:53 +0800

    when install service, auto start

--- eokan_svc.c
+++ eokan_svc.c
@@ -74,13 +74,206 @@ static int eokan_svc_install(void)
 		printf("CreateService failed (%d)\n", GetLastError());
 		CloseServiceHandle(schSCManager);
 		return -2;
+	} else {
+		printf("Service installed successfully\n");
 	}
-	else printf("Service installed successfully\n");
+
+	/* start service now */
+	if (!StartService(schService, 0, NULL)) {
+		fprintf(stderr, "start service failed.\n");
+	}
 
 	CloseServiceHandle(schService);
 	CloseServiceHandle(schSCManager);
 	return 0;
 }
+
+static BOOL __stdcall stop_depends(SC_HANDLE schSCManager, SC_HANDLE schService)
+{
+	DWORD i;
+	DWORD dwBytesNeeded;
+	DWORD dwCount;
+	LPENUM_SERVICE_STATUS   lpDependencies = NULL;
+	ENUM_SERVICE_STATUS     ess;
+	SC_HANDLE               hDepService;
+	SERVICE_STATUS_PROCESS  ssp;
+
+	DWORD dwStartTime = GetTickCount();
+	DWORD dwTimeout = 30000; // 30-second time-out
+
+	// Pass a zero-length buffer to get the required buffer size.
+	if ( EnumDependentServices( schService, SERVICE_ACTIVE,
+				lpDependencies, 0, &dwBytesNeeded, &dwCount ) ) {
+		// If the Enum call succeeds, then there are no dependent
+		// services, so do nothing.
+		return TRUE;
+	} else {
+		if ( GetLastError() != ERROR_MORE_DATA )
+			return FALSE; // Unexpected error
+
+		// Allocate a buffer for the dependencies.
+		lpDependencies = (LPENUM_SERVICE_STATUS) HeapAlloc(
+				GetProcessHeap(), HEAP_ZERO_MEMORY, dwBytesNeeded );
+
+		if ( !lpDependencies )
+			return FALSE;
+
+		// Enumerate the dependencies.
+		if ( !EnumDependentServices( schService, SERVICE_ACTIVE,
+					lpDependencies, dwBytesNeeded, &dwBytesNeeded,
+					&dwCount ) )
+			return FALSE;
+
+		for ( i = 0; i < dwCount; i++ ) {
+			ess = *(lpDependencies + i);
+			// Open the service.
+			hDepService = OpenService( schSCManager,
+					ess.lpServiceName,
+					SERVICE_STOP | SERVICE_QUERY_STATUS );
+
+			if ( !hDepService )
+				return FALSE;
+
+			// Send a stop code.
+			if ( !ControlService( hDepService,
+						SERVICE_CONTROL_STOP,
+						(LPSERVICE_STATUS) &ssp ) )
+				return FALSE;
+
+			// Wait for the service to stop.
+			while ( ssp.dwCurrentState != SERVICE_STOPPED )
+			{
+				Sleep( ssp.dwWaitHint );
+				if ( !QueryServiceStatusEx(
+							hDepService,
+							SC_STATUS_PROCESS_INFO,
+							(LPBYTE)&ssp,
+							sizeof(SERVICE_STATUS_PROCESS),
+							&dwBytesNeeded ) )
+					return FALSE;
+
+				if ( ssp.dwCurrentState == SERVICE_STOPPED )
+					break;
+
+				if ( GetTickCount() - dwStartTime > dwTimeout )
+					return FALSE;
+			}
+		}
+	}
+	if (lpDependencies)
+		HeapFree(GetProcessHeap(), 0, lpDependencies );
+	return TRUE;
+}
+
+static void __stdcall do_stop_eokansvc(SC_HANDLE schSCManager, SC_HANDLE schService)
+{
+    SERVICE_STATUS_PROCESS ssp;
+    DWORD dwStartTime = GetTickCount();
+    DWORD dwBytesNeeded;
+    DWORD dwTimeout = 30000; // 30-second time-out
+    DWORD dwWaitTime;
+
+    // Make sure the service is not already stopped.
+    if ( !QueryServiceStatusEx(
+            schService,
+            SC_STATUS_PROCESS_INFO,
+            (LPBYTE)&ssp,
+            sizeof(SERVICE_STATUS_PROCESS),
+            &dwBytesNeeded ) )
+    {
+        printf("QueryServiceStatusEx failed (%d)\n", GetLastError());
+        goto stop_cleanup;
+    }
+
+    if ( ssp.dwCurrentState == SERVICE_STOPPED )
+    {
+        printf("Service is already stopped.\n");
+        goto stop_cleanup;
+    }
+
+    // If a stop is pending, wait for it.
+
+    while ( ssp.dwCurrentState == SERVICE_STOP_PENDING )
+    {
+        printf("Service stop pending...\n");
+
+        // Do not wait longer than the wait hint. A good interval is
+        // one-tenth of the wait hint but not less than 1 second
+        // and not more than 10 seconds.
+
+        dwWaitTime = ssp.dwWaitHint / 10;
+
+        if( dwWaitTime < 1000 )
+            dwWaitTime = 1000;
+        else if ( dwWaitTime > 10000 )
+            dwWaitTime = 10000;
+
+        Sleep( dwWaitTime );
+
+        if ( !QueryServiceStatusEx(
+                 schService,
+                 SC_STATUS_PROCESS_INFO,
+                 (LPBYTE)&ssp,
+                 sizeof(SERVICE_STATUS_PROCESS),
+                 &dwBytesNeeded ) )
+        {
+            printf("QueryServiceStatusEx failed (%d)\n", GetLastError());
+            goto stop_cleanup;
+        }
+
+        if ( ssp.dwCurrentState == SERVICE_STOPPED )
+        {
+            printf("Service stopped successfully.\n");
+            goto stop_cleanup;
+        }
+
+        if ( GetTickCount() - dwStartTime > dwTimeout )
+        {
+            printf("Service stop timed out.\n");
+            goto stop_cleanup;
+        }
+    }
+
+    // If the service is running, dependencies must be stopped first.
+
+    stop_depends(schSCManager, schService);
+
+    // Send a stop code to the service.
+    if ( !ControlService(
+            schService,
+            SERVICE_CONTROL_STOP,
+            (LPSERVICE_STATUS) &ssp ) ) {
+        printf( "ControlService failed (%d)\n", GetLastError() );
+        goto stop_cleanup;
+    }
+
+    // Wait for the service to stop.
+
+    while ( ssp.dwCurrentState != SERVICE_STOPPED ) {
+        Sleep( ssp.dwWaitHint );
+        if ( !QueryServiceStatusEx(
+                schService,
+                SC_STATUS_PROCESS_INFO,
+                (LPBYTE)&ssp,
+                sizeof(SERVICE_STATUS_PROCESS),
+                &dwBytesNeeded ) ) {
+            printf( "QueryServiceStatusEx failed (%d)\n", GetLastError() );
+            goto stop_cleanup;
+        }
+
+        if ( ssp.dwCurrentState == SERVICE_STOPPED )
+            break;
+
+        if ( GetTickCount() - dwStartTime > dwTimeout ) {
+            printf( "Wait timed out\n" );
+            goto stop_cleanup;
+        }
+    }
+    printf("Service stopped successfully\n");
+stop_cleanup:
+	return;
+}
+
 
 static int __stdcall eokan_svc_remove(void)
 {
@@ -109,6 +302,8 @@ static int __stdcall eokan_svc_remove(void)
 		CloseServiceHandle(schSCManager);
 		return -2;
 	}
+
+	do_stop_eokansvc(schSCManager, schService);
 
 	// Delete the service.
 	if (! DeleteService(schService)) {
